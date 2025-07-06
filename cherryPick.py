7ecd9bc6d948266933fd48eea69401ed8182f8e2
sanalSaraswat <saraswatsanal@gmail.com>
2025-07-06 18:46:00 +0530

    Added code to cherryPick.py

--- cherryPick.py
+++ cherryPick.py
@@ -1,3 +1,14 @@
 import subprocess
 
-
+def cherryPick(branch, commitHash):
+    try:
+        subprocess.run(['git', 'checkout', '-b', branch])
+        print(f'Branch {branch} sucessfully created!')
+        subprocess.run(['git', 'cherry-pick', commitHash])
+        print(f"Successfully cherry-picked commit {commitHash}")
+        
+    except subprocess.CalledProcessError as e:
+        print(f"Error during cherry-pick: {e}")
+        
+        
+cherryPick('feature', '8e2e3b13318f0968e3462778444f836922e51efe' )
